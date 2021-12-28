4d7f4a56b3bcbe8be090e24c5da8ff07d51e245d
JosielMatos <josielmatos77@protonmail.com>
2021-12-28 18:00:43 -0300

    Add header and home html

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,14 @@
-export default function Header() {
-  // TODO
+import Link from 'next/link';
+import { ReactElement } from 'react';
+
+export default function Header(): ReactElement {
+  return (
+    <header>
+      <Link href="/">
+        <a>
+          <img src="logo.svg" alt="logo" />
+        </a>
+      </Link>
+    </header>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,10 @@
 import { GetStaticProps } from 'next';
+import Link from 'next/link';
+import { ReactElement } from 'react';
+
+import { FiCalendar, FiUser } from 'react-icons/fi';
+
+import Header from '../components/Header';
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -24,13 +30,36 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home(): ReactElement {
+  return (
+    <>
+      <main className={commonStyles.container}>
+        <Header />
+        <div className={styles.postsList}>
+          <Link href="/">
+            <a className={styles.post}>
+              <strong>Como Utilizar Hooks</strong>
+              <p>Pensando em sincronização em vez de ciclos de vida.</p>
+              <ul>
+                <li>
+                  <FiCalendar />
+                  15 Mar 2021
+                </li>
+                <li>
+                  <FiUser />
+                  Joseph Oliveira
+                </li>
+              </ul>
+            </a>
+          </Link>
+        </div>
+      </main>
+    </>
+  );
+}
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
-
-//   // TODO
-// };
+export const getStaticProps: GetStaticProps = async () => {
+  // const prismic = getPrismicClient();
+  // const postsResponse = await prismic.query(TODO);
+  // TODO
+};
